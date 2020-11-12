fe1d3ab88396c1532c715d639f9297a991619639
Wesley <wspencer3295@yahoo.com>
2020-11-12 15:16:43 -0700

    commit tree ba66efb0670bdbe344b5bc138607d16fbb6ce36e

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
+
+
+def my_each(words)
   # code here
+if block_given?
+   i = 0
+      while i < words.length
+    yield words[i]
+    i += 1
+      end
+words
+else
+   "No Block Given!"
+end
+
 end
